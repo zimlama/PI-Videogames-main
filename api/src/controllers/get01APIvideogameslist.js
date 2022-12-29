4bf5d03ab356943820dcc97c76cd1f7eaf8b3351
zimlama <zimlama@gmail.com>
2022-12-28 19:49:23 -0500

    2022-12-28v2.0 backend get,post,db

--- api/src/controllers/get01APIvideogameslist.js
+++ api/src/controllers/get01APIvideogameslist.js
@@ -3,23 +3,66 @@ require("dotenv").config();
 const { API_KEY, API_URL } = process.env;
 
 const get01APIlist = async () => {
-    //const apiWebINFO = await axios.get(`${API_URL}games?key=${API_KEY}&page_size=100`,{headers:{'Accept-Encoding':'identity'}},{ params: { limit: 100 }})
-    const apiWebINFO = await axios.get(`${API_URL}games?key=${API_KEY}&page_size=100`,{headers:{'Accept-Encoding':'identity'}})
-    .then((res) =>
-        res.data.results.map((video) => {
-            return {
-                id: video.id,
-                name: video.name,
-                slug: video.slug,
-                released: video.released,
-                rating: video.rating,
-                platforms: video.platforms.map(el => el.platform.name),
-                background_image: video.background_image,
-                genre: video.genres.map(el => el.name)
-            };
-        })
+    // debe ser asyncronico porque no sabes cuanto tarda la api en contestar
+    let apiInfo = [];
+  
+    const pageOne = await axios.get(`${API_URL}games?key=${API_KEY}&page=1`, {
+      headers: { "Accept-Encoding": "identity" },
+    }); // ?al colocar el page size toma un limite  de 40 ids no importa la cantidad que coloco
+    const pageTwo = await axios.get(`${API_URL}games?key=${API_KEY}&page=2`, {
+      headers: { "Accept-Encoding": "identity" },
+    });
+    const pageThree = await axios.get(`${API_URL}games?key=${API_KEY}&page=3`, {
+      headers: { "Accept-Encoding": "identity" },
+    });
+    const pageFour = await axios.get(`${API_URL}games?key=${API_KEY}&page=4`, {
+      headers: { "Accept-Encoding": "identity" },
+    });
+    const pageFive = await axios.get(`${API_URL}games?key=${API_KEY}&page=5`, {
+      headers: { "Accept-Encoding": "identity" },
+    });
+  
+    apiInfo = pageOne.data.results.concat(
+      pageTwo.data.results,
+      pageThree.data.results,
+      pageFour.data.results,
+      pageFive.data.results
     );
-  return apiWebINFO;
-};
+  
+    let apiInfoTotal = apiInfo.map((video) => {
+      return {
+        id: video.id,
+        name: video.name,
+        slug: video.slug,
+        released: video.released,
+        rating: video.rating,
+        platforms: video.platforms.map((el) => el.platform.name),
+        background_image: video.background_image,
+        genre: video.genres.map((el) => el.name),
+      };
+    });
+  
+    return apiInfoTotal;
+  };
+
+// const get01APIlist = async () => {
+//     //const apiWebINFO = await axios.get(`${API_URL}games?key=${API_KEY}&page_size=100`,{headers:{'Accept-Encoding':'identity'}},{ params: { limit: 100 }})
+//     const apiWebINFO = await axios.get(`${API_URL}games?key=${API_KEY}&page_size=100`,{headers:{'Accept-Encoding':'identity'}})
+//     .then((res) =>
+//         res.data.results.map((video) => {
+//             return {
+//                 id: video.id,
+//                 name: video.name,
+//                 slug: video.slug,
+//                 released: video.released,
+//                 rating: video.rating,
+//                 platforms: video.platforms.map(el => el.platform.name),
+//                 background_image: video.background_image,
+//                 genre: video.genres.map(el => el.name)
+//             };
+//         })
+//     );
+//   return apiWebINFO;
+// };
 
 module.exports = { get01APIlist };
